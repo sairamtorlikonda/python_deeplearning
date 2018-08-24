8635eb60a263280973e699a3f4b1738049987cf5
sairamtorlikonda <42658966+sairamtorlikonda@users.noreply.github.com>
2018-08-24 17:50:54 -0500

    first icp
    python icp1

---
+++ ICP1/source/firstICP.py
@@ -0,0 +1,3 @@
+f_name=input("enter the first name:")
+l_name=input("enter the last name:")
+print(f_name[::-1],l_name[::-1])
---
+++ ICP1/source/first_icp_1.py
@@ -0,0 +1,5 @@
+x=int(input("enter first number:"))
+y=int(input("enter second number:"))
+print("addition:",x+y)
+print("supperaction:",x-y)
+print("multiplication:",x*y)
